0c319d34978477c48d5f9305eef9be55dd6c2e8c
KingFarGrace <KingFarGrace.163.com>
2022-05-30 16:01:53 +0800

    Finish Several Pages - 完成了登录和注册页UI - 完成了主页的一部分UI - 添加了404页和503页（未实现跳转）

---
+++ src/components/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-row><h1>404 NOT FOUND</h1></el-row>
+    <el-row
+      ><el-button type="primary" icon="el-icon-arrow-left" @back="goBack"
+        >返回</el-button
+      ></el-row
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  methods: {
+    goBack() {
+      console.log("go back");
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+// import store from './store/store/js'
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // store,
   components: { App },
   template: '<App/>'
 })
