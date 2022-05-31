a738b5a572f14c119f182428899d865a31ba9db4
KingFarGrace <KingFarGrace.163.com>
2022-05-31 10:58:04 +0800

    UI changes - 在公告板组件中加入了走马灯图片展示 - 添加了404页和503页的返回按钮

--- src/components/SystemBulletin.vue
+++ src/components/SystemBulletin.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="grid-content bg-purple">
-    <el-tabs type="border-card">
-      <el-tab-pane label="系统公告标题1">系统公告1</el-tab-pane>
-      <el-tab-pane label="系统公告标题2">系统公告2</el-tab-pane>
-      <el-tab-pane label="系统公告标题3">系统公告3</el-tab-pane>
-      <el-tab-pane label="系统公告标题4">系统公告4</el-tab-pane>
-    </el-tabs>
+    <el-row
+      ><div margin="">
+        <el-tabs type="border-card">
+          <el-tab-pane label="系统公告标题1">系统公告1</el-tab-pane>
+          <el-tab-pane label="系统公告标题2">系统公告2</el-tab-pane>
+          <el-tab-pane label="系统公告标题3">系统公告3</el-tab-pane>
+          <el-tab-pane label="系统公告标题4">系统公告4</el-tab-pane>
+        </el-tabs>
+      </div></el-row
+    >
+    <el-row
+      ><el-carousel :interval="4000" type="card" height="200px">
+        <el-carousel-item v-for="item in 6" :key="item">
+          <h3 class="medium">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel></el-row
+    >
   </div>
 </template>
 
-<script>
-
-export default {
-    name: 'SystemBulletin'
+<style>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
 }
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
+
+<script>
+export default {
+  name: "SystemBulletin",
+};
 </script>
--- src/components/Unavailable.vue
+++ src/components/Unavailable.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row><h1>503 SERVICE UNAVAILABLE</h1></el-row>
     <el-row
-      ><el-button type="primary" icon="el-icon-arrow-left" @back="goBack"
+      ><el-button type="primary" icon="el-icon-arrow-left" @click="returnLast"
         >返回</el-button
       ></el-row
     >
@@ -13,8 +13,8 @@
 export default {
   name: "Unavailable",
   methods: {
-    goBack() {
-      console.log("go back");
+    returnLast() {
+      this.$router.go(-1);
     },
   },
 };
